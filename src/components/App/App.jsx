3ab49f90aa0b7b732bcd4af4c8a1b9e46a10b680
Bladi <bladimaltego@gmail.com>
2022-09-28 16:56:24 +0200

    feat: added component Functions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Button from '../Button/Button'
 import Functions from '../Functions/Functions'
 import MathOp from '../MathOp/MathOp'
+import Numbers from '../Numbers/Numbers'
 import Result from '../Result/Result'
 import './App.css'
 import i18n from './i18n.json'
@@ -9,31 +9,13 @@ import i18n from './i18n.json'
 
 
 const App = () => {
-    console.log("Renderizado App")
-    const clickHandlerFunction = text => {
-        console.log("Button.handleClick",text)
-    }
-
     return(
         <main className='react-calculator'>
             <h1>{i18n.Title}</h1>
             <Result value={undefined} />
-            <div className="numbers">
-                    <Button text={i18n.one} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.two} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.three} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.four} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.five} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.six} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.seven} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.eight} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.nine} handleOnClick={clickHandlerFunction}/>
-                    <Button text={i18n.cero} handleOnClick={clickHandlerFunction}/>
-            </div>
-          
+            <Numbers onClickNumber={(number) => console.log("onClickNumbers",number)}/>
             <Functions onContentClear={() => console.log("clear")} 
                        onDelete={() => console.log("delete")} />
-            
             <div className="math-operations">
                 <MathOp onClickOperation={operation => console.log(operation)} 
                         onClickEqual={equal => console.log(equal)}/>
--- src/components/Functions/Functions.jsx
+++ src/components/Functions/Functions.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Button from '../Button/Button'
 import PropTypes from 'prop-types'
 import i18n from './i18n.json'
+import './Functions.css'
 
 const Functions = ({onContentClear,onDelete}) => (
     <section className="functions">
-       <Button text={i18n.clear} handleOnClick={() => onContentClear()}></Button>
+       <Button type="button-long-text" text={i18n.clear} handleOnClick={() => onContentClear()}></Button>
        <Button text="&larr;" handleOnClick={() => onDelete()}></Button>
     </section>
   )  
