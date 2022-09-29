943b02345d7423b4f6f5b42a96f035cb88615040
Bladi <bladimaltego@gmail.com>
2022-09-29 13:40:05 +0200

    feat: updated App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+/* eslint no-eval:0 */
+import React,{useState} from 'react'
+import words from 'lodash.words'
 import Functions from '../Functions/Functions'
 import MathOp from '../MathOp/MathOp'
 import Numbers from '../Numbers/Numbers'
@@ -6,20 +8,24 @@ import Result from '../Result/Result'
 import './App.css'
 import i18n from './i18n.json'
 
-
-
 const App = () => {
+    const [stack,setStack] = useState('')
+    const items = words(stack,/[^-^+^*^/]/g)
+    console.log('items: ',items)
     return(
         <main className='react-calculator'>
             <h1>{i18n.Title}</h1>
-            <Result value={undefined} />
-            <Numbers onClickNumber={(number) => console.log("onClickNumbers",number)}/>
-            <Functions onContentClear={() => console.log("clear")} 
-                       onDelete={() => console.log("delete")} />
-            <div className="math-operations">
-                <MathOp onClickOperation={operation => console.log(operation)} 
-                        onClickEqual={equal => console.log(equal)}/>
-            </div>
+            <Result value={items[items.length -1]} />
+            <Numbers onClickNumber={number => setStack(`${stack}${number}`)}/>
+            <Functions onContentClear={() => setStack('')} 
+                       onDelete={() => {
+                        if(stack.length > 0){
+                            const newStack = stack.substring(0,stack.length -1)
+                            setStack(newStack) 
+                        }
+                       }} />
+            <MathOp onClickOperation={operation => setStack(`${stack}${operation}`)}
+                        onClickEqual={equal => setStack(eval(stack).toString())}/>
         </main>
     )
 }
--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Result.css'
 
-
-
-
 const Result = ({value}) => (
     <div className="result">
       <span>{value}</span>
